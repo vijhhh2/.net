4be34556b6bb5a92edef348326aec343b1770b7f
Vijay <vijay.rm@accenture.com>
2020-12-17 16:13:07 +0530

    testing dev

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace bookstore
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World From Dev!");
                 });
             });
         }
