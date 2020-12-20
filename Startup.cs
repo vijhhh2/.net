7d157d6dd3933f717b6bb8e68f55d37c1695d711
Vijay <vijay.rm@accenture.com>
2020-12-20 23:08:36 +0530

    Added Static Files

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace bookstore
@@ -26,6 +28,16 @@ namespace bookstore
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
+
+            // app.UseStaticFiles(
+            //     new StaticFileOptions()
+            //     {
+            //         FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "customstaticfiles")),
+            //         RequestPath = "/customstaticfiles"
+            //     }
+            // );
 
             app.UseRouting();
 
